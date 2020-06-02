be008e6d581c4c5fc9c28b76aad83469480099f3
William <will.kof1@gmail.com>
2020-06-01 22:42:05 -0300

    pyzumbis

--- exerc/read_calc_day_hours_min_seg.py
+++ exerc/read_calc_day_hours_min_seg.py
@@ -5,5 +5,5 @@ dias=int(input('quantos dias: ', ))
 horas=int(input('quantas horas: '))
 minutos=int(input('quantos minutos: '))
 segundos=int(input('quantos segundos: '))
-total = dias*24*60*60 + horas*60*60 + minutos*60 + segundos
+total = (dias*24*60*60) + (horas*60*60) + (minutos*60 + segundos)
 print('quantidade de segundos é: ', total)
